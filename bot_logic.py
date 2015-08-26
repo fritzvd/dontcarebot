799b8fffc822e58bf8576c45aac92f1f227f11e4
fritzvd <fritz.vandeventer@nelen-schuurmans.nl>
2015-08-26 14:17:06 +0200

    friendly replies, favorites

--- bot_logic.py
+++ bot_logic.py
@@ -3,11 +3,14 @@ import json
 import os
 import random
 
+
 def auth():
     os.chdir('/home/fritz/Development/me/dontcarebot/')
     with open("access.json", 'r') as f:
         db = json.load(f)
-    return Twython(db["API_Key"], db["API_Secret"], db["Access_Token"], db["Access_Token_Secret"])
+    return Twython(db["API_Key"], db["API_Secret"], db["Access_Token"],
+                   db["Access_Token_Secret"])
+
 
 def load():
     with open("queue.json", 'r') as f:
@@ -16,29 +19,47 @@ def load():
         info = json.load(fi)
     return queue, info
 
+
 def dump(queue, info):
     with open("queue.json", 'w') as f:
         json.dump(queue, f)
     with open("info.json", 'w') as fi:
         json.dump(info, fi)
 
-def respond(twitter, top_tweet):
+
+def respond(twitter, top_tweet, reply=False):
     name = top_tweet["user"]["screen_name"]
-    twitter.create_friendship(screen_name=name)
-    with open("messages.json", 'r') as f:
+    # twitter.create_friendship(screen_name=name)
+    message_file = "messages.json" 
+    if reply:
+        message_file = "friendly_replies.json"
+    with open(message_file, 'r') as f:
         messages = json.load(f)
     message = messages[random.randint(0, len(messages) - 1)]['message']
-    twitter.update_status(status="@%s %s" %(name, message), in_reply_to_status_id=top_tweet["id"])
+    twitter.update_status(status="@%s %s" % (name, message),
+                          in_reply_to_status_id=top_tweet["id"])
 
 
 def main():
     twitter = auth()
     queue, info = load()
-    tweets = twitter.search(q="tired sad", result_type="recent", since_id=info["sinceid"], count='100')
+    mentions = twitter.get_mentions_timeline()
+
+    for mention in mentions:
+        if not mention['favorited']:
+            twitter.create_favorite(id=mention['id'])
+            respond(twitter, mention, reply=True)
+
+    tweets = twitter.search(q="tired sad", result_type="recent",
+                            since_id=info["sinceid"], count='100')
     info["sinceid"] = tweets["search_metadata"]["max_id"]
     triggers = ("tired", "sad")
-    to_add = [tweet for tweet in tweets["statuses"] if not tweet["retweeted"] and not tweet.has_key("retweeted_status")]
-    to_add = [tweet for tweet in to_add if tweet["text"].startswith(triggers) or tweet["text"].split(" ",1)[1].startswith(triggers)]
+    to_add = [tweet for tweet in tweets["statuses"]
+              if not tweet["retweeted"]
+              and "retweeted_status" not in tweet]
+    to_add = [tweet for tweet in to_add
+              if triggers[0] in tweet["text"]
+              or triggers[1] in tweet["text"]]
     queue = queue + to_add
     mx = max(len(to_add), 20)
     if len(queue) > mx:
@@ -47,6 +68,7 @@ def main():
         respond(twitter, queue.pop())
     dump(queue, info)
 
-#run on cron every minute
+
+# run on cron every minute
 if __name__ == "__main__":
     main()
